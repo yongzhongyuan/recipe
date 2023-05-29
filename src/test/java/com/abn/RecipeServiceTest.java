424aa2f6f6c9cf94a03b45c60201e20e58ec0d97
Yongzhong Yuan <leeyansoft@gmail.com>
2023-05-29 12:54:37 +0200

    add unit test

--- src/test/java/com/abn/RecipeServiceTest.java
+++ src/test/java/com/abn/RecipeServiceTest.java
@@ -1,2 +1,48 @@
-package com.abn;public class RecipeServiceTest {
+package com.abn;
+
+import com.abn.model.Recipe;
+import com.abn.repository.RecipeRepository;
+import com.abn.service.RecipeService;
+import com.abn.service.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceTest {
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @InjectMocks
+    private RecipeService recipeService = new RecipeServiceImpl();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllRecipes_ReturnsAllRecipes() {
+        // Arrange
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+        when(recipeRepository.findAll()).thenReturn(recipes);
+
+        // Act
+        List<Recipe> result = recipeService.getAllRecipes();
+
+        // Assert
+        assertEquals(2, result.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
 }
+
